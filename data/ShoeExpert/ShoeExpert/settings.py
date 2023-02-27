065fbcdc113c7003da1a8b0002c11a493df700f5
Max_McVay <mhmcvay@csuchico.edu>
2023-02-27 12:39:16 -0800

    updated allowed hosts

--- data/ShoeExpert/ShoeExpert/settings.py
+++ data/ShoeExpert/ShoeExpert/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
