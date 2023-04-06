f7af34dfd212f36ea1d30e979267825160280d48
Maxwell Kapral <git@maxkapral.com>
2023-04-06 15:45:12 -0700

    csv example and Model example added

--- PERSIST_SHOES_DEV-TMP/tmp.py
+++ PERSIST_SHOES_DEV-TMP/tmp.py
@@ -1,40 +1,35 @@
 from aggregate import ColumnSelector, Gender, Url_Paths, ScraperSingleton
 
 def main():
-  i = 1
-  j = 2
-  while i < 41:
-    ScraperSingleton().scrape(
-      filename = f"running_shoes_{i}.csv",
-      columnlist = [
-        ColumnSelector.ARCH_TYPE,
-        ColumnSelector.BRAND,
-        ColumnSelector.CUSHIONING,
-        ColumnSelector.DISTANCE,
-        ColumnSelector.FEATURES,
-        ColumnSelector.FLEXIBILITY,
-        ColumnSelector.FOREFOOT_HEIGHT,
-        ColumnSelector.HEEL_HEIGHT,
-        ColumnSelector.HEEL_TOE_DROP,
-        ColumnSelector.MSRP,
-        ColumnSelector.PRONATION,
-        ColumnSelector.RELEASE_DATE,
-        ColumnSelector.STRIKE_PATTERN,
-        ColumnSelector.TECHNOLOGY,
-        ColumnSelector.TERRAIN,
-        ColumnSelector.TOEBOX,
-        ColumnSelector.USE,
-        ColumnSelector.WATERPROOFING,
-        ColumnSelector.WEIGHT,
-        ColumnSelector.WIDTH
-      ],
-      url_path=Url_Paths.RUNNING_SHOES,
-      pages=range(i, j),
-      sleep=0,
-      timeout=1
-    )
-    i = j
-    j += 1
+  ScraperSingleton().scrape(
+    filename = f"/dev/shm/running_shoes.csv", # FIXME:
+    columnlist = [
+      ColumnSelector.ARCH_TYPE,
+      ColumnSelector.BRAND,
+      ColumnSelector.CUSHIONING,
+      ColumnSelector.DISTANCE,
+      ColumnSelector.FEATURES,
+      ColumnSelector.FLEXIBILITY,
+      ColumnSelector.FOREFOOT_HEIGHT,
+      ColumnSelector.HEEL_HEIGHT,
+      ColumnSelector.HEEL_TOE_DROP,
+      ColumnSelector.MSRP,
+      ColumnSelector.PRONATION,
+      ColumnSelector.RELEASE_DATE,
+      ColumnSelector.STRIKE_PATTERN,
+      ColumnSelector.TECHNOLOGY,
+      ColumnSelector.TERRAIN,
+      ColumnSelector.TOEBOX,
+      ColumnSelector.USE,
+      ColumnSelector.WATERPROOFING,
+      ColumnSelector.WEIGHT,
+      ColumnSelector.WIDTH
+    ],
+    url_path=Url_Paths.RUNNING_SHOES,
+    pages=range(1, 5),
+    sleep=0.01,
+    timeout=1
+  )
 
 if __name__ == "__main__":
     main()
