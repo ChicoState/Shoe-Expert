e8210055e891ca607e88d723edb4f229f679d54b
hltiffany@csuchico.edu <hltiffany@csuchico.edu>
2023-05-05 09:26:06 -0700

    Working on filter page

--- data/ShoeExpert/app1/views.py
+++ data/ShoeExpert/app1/views.py
@@ -113,16 +113,20 @@ def blog(request):
     return render(request, 'app1/blog.html')
 
 @login_required(login_url='/login/')
-def filter(request):
-    return(request, 'app1/filer.html')
+def filter(request, userShoe, userCushion):
+   shoes_per_page = 5
+      queryset = RunningShoe.objects.filter(shoe=userShoe)
+   paginator = Paginator(queryset, shoes_per_page)
+   page = request.GET.get('page')
+   shoes = paginator.get_page(page)
+   return render(request, 'app1/filter.html', {'shoes': shoes})
 
 @login_required(login_url='/login/')
 def filter2(request):
     if (request.method == "POST"):
         filter_form = FilterForm(request.POST)
         if filter_form.is_valid():
-           userBrand = filter_form.cleaned_data["Brand"]
-           userCushion = filter_form.cleaned_data["Cushioning"]
-           return redirect(filter, userBrand, userCushion)
+           userShoe = filter_form.cleaned_data["Brand"]
+           return redirect(filter, userShoe)
     return render(request, 'app1/filter2.html', {'filter_form': FilterForm})
 
